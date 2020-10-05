50231abeeb12ed75196141f0aaf23429d3ea3c9d
Egeniy Tarasov <tarasov@ucontent.ru>
2020-10-05 19:50:12 +0300

    Initial Commit

---
+++ NetworkApp (MVVM)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkApp (MVVM)
+//
+//  Created by Евгений Тарасов on 05.10.2020.
+//  Copyright © 2020 Евгений Тарасов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
